9390a29b67953dfe22537f52411b8295d13b665c
Clayton Priestley <clayton.priestley@gmail.com>
2023-02-22 13:50:26 -0600

    Create Manager Interface - add empty implementation of interface

---
+++ src/main/java/service/implementations/ContactManager.java
@@ -0,0 +1,33 @@
+package service.implementations;
+
+import data.Contact;
+import service.Manager;
+
+import java.util.List;
+
+public class ContactManager implements Manager {
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void printMenu() {
+
+    }
+
+    @Override
+    public void addContact(Contact contact) {
+
+    }
+
+    @Override
+    public List<Contact> getContacts() {
+        return null;
+    }
+
+    @Override
+    public void searchContacts(String name) {
+
+    }
+}
