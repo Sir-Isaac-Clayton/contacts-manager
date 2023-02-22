04f87c06da8c180fc017d222b7f1a04635c14d9a
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-22 17:01:12 -0600

    added importContacts method

--- src/main/java/data/ContactDatabase.java
+++ src/main/java/data/ContactDatabase.java
@@ -1,13 +1,36 @@
 package data;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDatabase {
     List<Contact> contacts;
 
+
     public ContactDatabase() {
         this.contacts = new ArrayList<>();
+        importContacts();
+    }
+
+    private void importContacts() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("contacts.txt"));
+            int counter = 0;
+            for (String line : lines) {
+               String [] arr = line.split(",");
+               counter += 1;
+               contacts.add(new Contact(arr[0], arr[1]));
+            }
+//            System.out.format("%d imported into database", counter);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
     public void addAllContacts(List<Contact> contacts) {
