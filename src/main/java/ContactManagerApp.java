322d55a2f8b7771785a5403ccf8fd6a7640c2e22
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-22 12:16:47 -0600

    contact manager app set up skeleton for app added while loop

--- src/main/java/ContactManagerApp.java
+++ src/main/java/ContactManagerApp.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
+
 public class ContactManagerApp {
+    public static int getUserInput(){
+        Scanner Input = new Scanner(System.in);
+        System.out.println("what would you like to do?\n");
+
+        System.out.println("1 - View contacts.");
+        System.out.println("2 - Add a new contact.");
+        System.out.println("3 - Search a contact by name.");
+        System.out.println("4 - Delete an existing contact.");
+        System.out.println("5 - exit");
+        return Input.nextInt();
+
+    }
+
+    private static void doChoice(int choice){
+        switch (choice) {
+//            case 1 -> getAllMovies();
+//            case 2 -> getMoviesByCategory("animated");
+//            case 3 -> getMoviesByCategory("drama");
+//            case 4 -> getMoviesByCategory("horror");
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+        while(true){
+            int choice = getUserInput();
+
+            doChoice(choice);
+
+            if (choice == 5){
+                break;
+            }
+
+        }
+    }
 }
